c1d5750a2d0cf813443d3309430b7c5c15283946
ewemormartin <martinewemor@gmail.com>
2024-05-30 12:05:31 +0200

    .

--- Kartya.js
+++ Kartya.js
@@ -4,24 +4,40 @@ export default class Kartya{
     #kep="";
     #allapot=true; // true akkor lathato, false akkor háttér
     #szuloElem;
+    #kepElem;
     constructor(kep, szuloElem){
         this.#kep=kep;
         this.#szuloElem=szuloElem;
         this.#allapot=false;
         this.megjelenit()
+        this.#kepElem=this.#szuloElem.childred("div:last-child").childred("img")
+        this.#kepElem.on("click",()=>{
+            this.setAllapot(true)
+        })
 
     }
     megjelenit(){
         let txt=`
-                <div class="card col-sm 3">
-                    <img src="${this.#kep}" alt="kép">
+                <div class="card col-sm-3">
+                    <img src="kepek/hatter.jpg" alt="">
                 </div>
         `
         this.#szuloElem.append(txt);
     }
+    setAllapot(ertek){
+        if (ertek==true || ertek ==false) {
+            this.#allapot = ertek
+            this.felfordit();
+        }
+        
+    }
 
     felfordit(){
-
+        if (this.#allapot) {
+            this.#kepElem.attr("src",this.#kep)
+        }else{
+            this.#kepElem.attr("src", "kepek/hatter.jpg")
+        }
     }
 
 }
